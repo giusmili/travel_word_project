d0541fefea3e6c4f56a23ca1d723dfb531b45e2f
giusmili <giusmili67@gmail.com>
2023-01-20 17:53:13 +0100

    js date

--- js/app.js
+++ js/app.js
@@ -7,14 +7,16 @@ document.addEventListener("DOMContentLoaded", e => {
         date_input_to: document.getElementsByName("to")
     };
 
-    //tester 
+    //tester le formulaire et ajouter si la date de début est supérieur à la date de fin
     User_travel.btn_submit.addEventListener("click", e => {
         e.preventDefault();
-        User_travel.date_input_from[0].value && User_travel.date_input_to[0].value ? Swal.fire('Good job!', 'You clicked the button!', 'success') : Swal.fire({
+        User_travel.date_input_from[0].value && User_travel.date_input_to[0].value && User_travel.date_input_to[0].value >= User_travel.date_input_from[0].value ? Swal.fire('Good choice!', 'You good check!', 'success') : Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Something went wrong!',
+            text: 'Check your dates',
             footer: '<a href="">Why do I have this issue?</a>'
         });
+        // Si la date d'arrivé est inférieur à la date de départ
+
     });
 });
